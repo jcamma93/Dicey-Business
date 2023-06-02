43cb179a81b731b4d00030acfe7d959fc59d41a3
jcamma93 <130416226+jcamma93@users.noreply.github.com>
2023-06-02 07:34:17 -0400

    Add files via upload

---
+++ DiceyBusiness.js
@@ -0,0 +1,62 @@
+let dice = [];
+let btnCreate = $("#btn-create");
+let btnRoll = $("#btn-roll");
+let btnSum = $("#btn-sum");
+
+$(btnCreate).click(() => {
+    let die = new Die();
+});
+
+$(btnRoll).click(() => {
+    for (let i = 0; i < dice.length; i++) {
+        dice[i].roll();
+    }
+});
+
+$(btnSum).click(() => {
+    let sum = 0;
+    for (let i = 0; i < dice.length; i++) {
+        sum += dice[i].value;
+    }
+    alert(`The sum of all dice on screen = ${sum}.`);
+});
+
+class Die {
+    constructor() {
+        this.div = $("<div></div>");
+        this.roll();
+        $(this.div).appendTo("#dice-container");
+        dice.push(this);
+        $(this.div).click(() => {
+            this.roll();
+        });
+        $(this.div).dblclick(() => {
+            $(this.div).remove();
+            dice.splice($.inArray(this, dice), 1);
+        });
+    }
+
+    roll() {
+        this.value = Math.floor(Math.random() * 6 + 1);
+        this.getChar();
+        $(this.div).text("" + this.char + "");
+        $(this.div).addClass("die");
+    }
+
+    getChar() {
+        if (this.value === 1) {
+            this.char = "⚀";
+        } else if (this.value === 2) {
+            this.char = "⚁"
+        } else if (this.value === 3) {
+            this.char = "⚂";
+        } else if (this.value === 4) {
+            this.char = "⚃";
+        } else if (this.value === 5) {
+            this.char = "⚄";
+        } else {
+            this.char = "⚅";
+        }
+    }
+}
+
